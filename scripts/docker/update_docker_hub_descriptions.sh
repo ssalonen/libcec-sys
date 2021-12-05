c415c9235c5b1d8fcec7ec1c91a134d16519a68e
Sami Salonen <ssalonen@gmail.com>
2021-12-05 08:44:11 +0200

    utility script: utility script to update dockerhub README
    Avoid using aemdesign/dockerhub-description, seems to be broken now due
    to whitespace issues in dockerhub API.
    
    Signed-off-by: Sami Salonen <ssalonen@gmail.com>

--- scripts/docker/update_docker_hub_descriptions.sh
+++ scripts/docker/update_docker_hub_descriptions.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 set -e
 
-[ -z $1 ] && echo "Give fully qualified image name as parameter"
-[ -z $DOCKERHUB_USERNAME ] && echo "Dockerhub username?" && read -s DOCKERHUB_USERNAME
-[ -z $DOCKERHUB_PASSWORD ] && echo "Dockerhub password?" && read -s DOCKERHUB_PASSWORD
+[ -z "$1" ] && echo "Give fully qualified image name as parameter"
+[ -z "$DOCKERHUB_USERNAME" ] && echo "Dockerhub username?" && read -s DOCKERHUB_USERNAME
+[ -z "$DOCKERHUB_PASSWORD" ] && echo "Dockerhub password?" && read -s DOCKERHUB_PASSWORD
 
-docker run --rm \
--v $(pwd)/README.md:/data/README.md:ro,Z \
-aemdesign/dockerhub-description \
-    $DOCKERHUB_USERNAME \
-    $DOCKERHUB_PASSWORD \
-    $1
-    
+REPO=$1
+README=$(pwd)/README.md
+TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'"${DOCKERHUB_USERNAME}"'", "password": "'${DOCKERHUB_PASSWORD}'"}' https://hub.docker.com/v2/users/login/ | jq -r .token)
+RESPONSE=$(curl -s --write-out %{response_code} --output /dev/null -H "Authorization: JWT ${TOKEN}" -X PATCH --data-urlencode full_description@${README} https://hub.docker.com/v2/repositories/${REPO}/)
+
+
+if [[ ${RESPONSE} -eq 200 ]]; then
+    exit 0
+else
+	echo "Error with repo ${REPO}: ${RESPONSE}"
+    exit 1
+fi
