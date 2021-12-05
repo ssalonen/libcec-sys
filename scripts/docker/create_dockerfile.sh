a1d949035222c57465ba34abb1919070be32c210
Sami Salonen <ssalonen@gmail.com>
2021-12-05 08:43:29 +0200

    CI: update cross docker images to 0.2.1
    This should bring newer cmake as well, to resolve #5.
    
    Signed-off-by: Sami Salonen <ssalonen@gmail.com>

--- scripts/docker/create_dockerfile.sh
+++ scripts/docker/create_dockerfile.sh
@@ -1,5 +1,5 @@
 cat << EOF > $1/Dockerfile
 FROM rustembedded/cross:$1-$2
 
-RUN apt-get remove -y libudev1 udev libudev-dev || :
+RUN apt-get remove --allow-remove-essential -y libudev1 udev libudev-dev || :
 EOF
