734287572ba9d7184efdd8b9186b8c8d84544421
Sami Salonen <ssalonen@gmail.com>
2022-02-01 19:54:17 +0200

    tests asserting ABI and cfg
    Signed-off-by: Sami Salonen <ssalonen@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -12,3 +12,27 @@ mod lib_abi6;
 pub use crate::lib_abi6::*;
 #[cfg(not(any(abi4, abi5, abi6)))]
 compile_error!("BUG: libcec abi not detected");
+
+#[cfg(test)]
+mod tests {
+
+    use crate::CEC_LIB_VERSION_MAJOR;
+
+    #[cfg(abi4)]
+    #[test]
+    fn test_abi4() {
+        assert_eq!(CEC_LIB_VERSION_MAJOR, 4);
+    }
+
+    #[cfg(abi5)]
+    #[test]
+    fn test_abi5() {
+        assert_eq!(CEC_LIB_VERSION_MAJOR, 5);
+    }
+
+    #[cfg(abi6)]
+    #[test]
+    fn test_abi6() {
+        assert_eq!(CEC_LIB_VERSION_MAJOR, 6);
+    }
+}
