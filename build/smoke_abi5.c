37c7599919d5b736baf9afe06ea9d6a30c24fe1c
Sami Salonen <ssalonen@gmail.com>
2022-07-20 13:34:00 +0300

    CI: fix smoke test building (#16)
    * CI: fix smoke test building
    
    cc_cmd was reused between runs, leading to unexpected results
    Improved build.rs debug logging
    
    * Changelog

--- build/smoke_abi5.c
+++ build/smoke_abi5.c
@@ -3,6 +3,7 @@
 
 int main()
 {
-    _Static_assert(CEC_LIB_VERSION_MAJOR == 5, "LIBCEC != v5.x.y");
+    _Static_assert(CEC_LIB_VERSION_MAJOR == 5, 
+        "libcec major version is " CEC_LIB_VERSION_MAJOR_STR ", not as expected (5)");
     return (intptr_t)libcec_initialise;
 }
