975966cefa3e07ae99b66302e052e5b40e3fa261
Sami Salonen <ssalonen@gmail.com>
2023-07-05 22:19:14 +0300

    [WIP] CI Windows: build in defined working directory (#39)
    * CI Windows: build in defined working directory
    
    Signed-off-by: Sami Salonen <ssalonen@gmail.com>
    
    * Do not define unused constants on win
    
    Signed-off-by: Sami Salonen <ssalonen@gmail.com>
    
    * changelog entry for windows fix
    
    Signed-off-by: Sami Salonen <ssalonen@gmail.com>
    
    ---------
    
    Signed-off-by: Sami Salonen <ssalonen@gmail.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## 4.0.2
 
 - build script to switch to `dircpy`, dropping dependency unmaintaned `copy_dir`
+- build script fixed for windows
 
 ## 4.0.1
 
--- build/build.rs
+++ build/build.rs
@@ -8,8 +8,10 @@ use std::io::BufReader;
 use std::path::{Path, PathBuf};
 use std::process::Command;
 
+#[cfg(not(target_os = "windows"))]
 const P8_PLATFORM_ROOT_ENV: &str = "p8-platform_ROOT";
 const LIBCEC_BUILD: &str = "libcec_build";
+#[cfg(not(target_os = "windows"))]
 const PLATFORM_BUILD: &str = "platform_build";
 const LIBCEC_SRC: &str = "vendor";
 
@@ -108,7 +110,9 @@ fn compile_vendored_libcec(dst: &Path) {
 fn compile_vendored_libcec(dst: &Path) {
     // All the compilation steps are combined into one command.
     println!("build libcec");
+    let libcec_build = dst.join(LIBCEC_BUILD);
     Command::new("cmd")
+        .current_dir(&dst.join(LIBCEC_SRC).join("project"))
         .arg("/C")
         .arg(dst.join(LIBCEC_SRC).join("windows").join("build-lib.cmd"))
         .arg(ARCHITECTURE)
@@ -118,7 +122,7 @@ fn compile_vendored_libcec(dst: &Path) {
             "Release"
         })
         .arg("2019")
-        .arg(dst.join(LIBCEC_BUILD))
+        .arg(&libcec_build)
         .arg("nmake")
         .status()
         .expect("failed to build libcec!");
