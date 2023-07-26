ed2858fed3056e8df14ae1fe09860f75ad16bb8b
Sami Salonen <ssalonen@gmail.com>
2023-07-26 17:23:22 +0300

    re-run build on cmake env flag changes
    Signed-off-by: Sami Salonen <ssalonen@gmail.com>

--- build/build.rs
+++ build/build.rs
@@ -367,6 +367,9 @@ fn main() {
     println!("cargo:rerun-if-env-changed=LIB");
     println!("cargo:rerun-if-env-changed=CL");
     println!("cargo:rerun-if-env-changed=_CL_");
+    println!("cargo:rerun-if-env-changed={CMAKE_C_COMPILER_LAUNCHER_ENV_VARIABLE}");
+    println!("cargo:rerun-if-env-changed={CMAKE_CXX_COMPILER_LAUNCHER_ENV_VARIABLE}");
+    
 
     // Try discovery using pkg-config
     if !cfg!(feature = "vendored") {
