aa0b9e987ef048d2a4160923a98c0a6d5b753a9c
Sami Salonen <ssalonen@gmail.com>
2024-11-30 23:15:50 +0200

    Update cec_build_prep.sh

--- docker/cec_build_prep.sh
+++ docker/cec_build_prep.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -yq libssl-dev
+apt-get install -yq libssl-dev libssl3
 # let's remove libudev, it causes only trouble in CI
 # when building libcec
 apt-get remove --allow-remove-essential -y libudev1 udev libudev-dev || :
