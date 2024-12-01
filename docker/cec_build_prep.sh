f2a0ca0a3a513670f5a43ef08a159784a1285ffc
Sami Salonen <ssalonen@gmail.com>
2024-12-01 08:45:12 +0200

    Update cec_build_prep.sh

--- docker/cec_build_prep.sh
+++ docker/cec_build_prep.sh
@@ -2,4 +2,5 @@
 
 # let's remove libudev, it causes only trouble in CI
 # when building libcec
+apt-get install -y libclang-dev
 apt-get remove --allow-remove-essential -y libudev1 udev libudev-dev || :
