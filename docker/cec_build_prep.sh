bfee1a81304d50919e88f7d39e3f358857352f54
Sami Salonen <ssalonen@gmail.com>
2024-12-01 10:47:02 +0200

    Clang11 also

--- docker/cec_build_prep.sh
+++ docker/cec_build_prep.sh
@@ -3,5 +3,5 @@
 # let's remove libudev, it causes only trouble in CI
 # when building libcec
 #apt-get install -y libclang-dev:$CROSS_DEB_ARCH
-apt-get update && apt-get install --assume-yes --no-install-recommends libclang-10-dev clang-10
+apt-get update && apt-get install --assume-yes --no-install-recommends libclang-10-dev libclang-11-dev clang-10 clang-11
 apt-get remove --allow-remove-essential -y libudev1 udev libudev-dev || :
